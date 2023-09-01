7e01c4c4615f971a811853dd2c0e4be5268a7ab2
Sahil Shefeek <sahilms345@gmail.com>
2023-09-01 20:45:59 +0530

    fix bug

--- greatestOfTwo.java
+++ greatestOfTwo.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class greatestOfTwo {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner("System.in");
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter two numbers :");
         int a = scanner.nextInt();
         int b = scanner.nextInt();
