01beb9f6fefd7bad47955c80adc606014652279f
Sahil Shefeek <sahilms345@gmail.com>
2023-09-01 22:18:53 +0530

    program++

---
+++ factorial.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class factorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number :");
+        int num = scanner.nextInt();
+        int factorial = 1;
+        int temp = num;
+        while (temp != 0) {
+            factorial = factorial * temp;
+            temp--;
+        }
+        System.out.println("The factorial of " + num + " is " + factorial);
+    }
+}
