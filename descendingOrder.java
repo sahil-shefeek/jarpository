541c5540e3f31a58b591a1fd15b31e3b529500f4
Sahil Shefeek <sahilms345@gmail.com>
2023-09-01 20:55:16 +0530

    program++

---
+++ descendingOrder.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class descendingOrder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the limit :");
+        int num = scanner.nextInt();
+        int i = num;
+        while (i > 0) {
+            System.out.println(i);
+            i--;
+        }
+    }
+}
