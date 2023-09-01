bad5f7174f255e3079ba13989de4ebc1397ece07
Sahil Shefeek <sahilms345@gmail.com>
2023-09-01 21:01:16 +0530

    program++

---
+++ sumOfN.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class sumOfN {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int temp, digit, sum = 0;
+        System.out.println("Enter any number :");
+        int num = scanner.nextInt();
+        temp = num;
+        while (temp != 0) {
+            digit = temp % 10;
+            sum += digit;
+            temp /= 10;
+        }
+        System.out.println("The sum of digits of " + num + " is " + sum);
+    }
+}
