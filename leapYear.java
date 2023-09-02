bab5a203116afba15ae406afa05ef8f88906a99a
Sahil Shefeek <sahilms345@gmail.com>
2023-09-02 16:13:26 +0530

    program++

---
+++ leapYear.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class leapYear {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the year :");
+        int year = scanner.nextInt();
+        if ((year % 100) == 0) {
+            if ((year % 400) == 0)
+                System.out.println(year + " is a leap year");
+            else
+                System.out.println(year + " is not a leap year!");
+        } else {
+            if ((year % 4) == 0)
+                System.out.println(year + " is a leap year!");
+            else
+                System.out.println(year + " is not a leap year!");
+        }
+    }
+}
