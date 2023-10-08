b4c71eadadc5268811b026afd127caa61c97003a
Sahil Shefeek <sahilms345@gmail.com>
2023-10-08 13:05:19 +0530

    program++

---
+++ arrays/matrix_add.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class matrix_add {
+    public static void main(String[] args) {
+        int rows, cols;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of rows and columns of matrix:\n");
+        rows = scanner.nextInt();
+        cols = scanner.nextInt();
+
+        int[][] mat1 = new int[rows][cols];
+        int[][] mat2 = new int[rows][cols];
+        int[][] sum = new int[rows][cols];
+
+        System.out.println("Enter the elements of 1st matrix:\n");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++)
+                mat1[i][j] = scanner.nextInt();
+        }
+        System.out.println("Enter the elements of 2nd matrix:\n");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++)
+                mat2[i][j] = scanner.nextInt();
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++)
+                sum[i][j] = mat1[i][j] + mat2[i][j];
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++)
+                System.out.printf("%d ", sum[i][j]);
+            System.out.println();
+        }
+    }
+}
