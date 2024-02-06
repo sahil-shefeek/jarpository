6dbfd08a73329e52bcdff22e4f5237091a5c3e3e
Sahil Shefeek <sahilms345@gmail.com>
2024-02-06 23:15:27 +0530

    Major Update

--- gui/SimpleCalculator.java
+++ gui/SimpleCalculator.java
@@ -1,35 +1,23 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 class GUI implements ActionListener {
     JFrame frame;
+    JPanel buttonsPanel;
     JTextField textField;
-    JButton addButton;
-    JButton subtractButton;
-    JButton multiplyButton;
-    JButton divideButton;
-    JButton modulusButton;
-    JButton equalsButton;
-    JButton clearButton;
-    private boolean isReset = true;
+    JButton addButton, subtractButton, multiplyButton, divideButton, modulusButton, equalsButton, clearButton,
+            deleteButton;
 
     GUI() {
-
         frame = new JFrame("Simple Calculator");
-        frame.setSize(360, 480);
+        frame.setSize(480, 640);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new GridLayout(6, 3));
-
-        textField = new JTextField(25);
-        textField.setBounds(50, 25, 300, 50);
-
-        JButton[] digitButtons = new JButton[10];
-        for (int i = 0; i < 10; i++) {
-            digitButtons[i] = new JButton(String.valueOf(i));
-            digitButtons[i].addActionListener(this);
-        }
-
+        frame.setLayout(null);
+        textField = new JTextField(32);
+        textField.setBounds(0, 0, 480, 64);
         addButton = new JButton("+");
         subtractButton = new JButton("-");
         multiplyButton = new JButton("*");
@@ -37,6 +25,7 @@ class GUI implements ActionListener {
         modulusButton = new JButton("%");
         equalsButton = new JButton("=");
         clearButton = new JButton("AC");
+        deleteButton = new JButton("Del");
         clearButton.setForeground(Color.RED);
         addButton.addActionListener(this);
         subtractButton.addActionListener(this);
@@ -44,81 +33,113 @@ class GUI implements ActionListener {
         divideButton.addActionListener(this);
         modulusButton.addActionListener(this);
         equalsButton.addActionListener(this);
-        clearButton.addActionListener(this);
+        clearButton.addActionListener(e -> textField.setText(""));
+        deleteButton.addActionListener(e -> {
+            String str = textField.getText();
+            if (str != null && str.length() > 0)
+                textField.setText(str.substring(0, str.length() - 1));
+        });
+        buttonsPanel = new JPanel();
+        buttonsPanel.setBounds(0, 64, 480, 540);
+        buttonsPanel.setLayout(new GridLayout(6, 3, 5, 5));
+        buttonsPanel.add(clearButton);
+        buttonsPanel.add(deleteButton);
+        buttonsPanel.add(addButton);
+        buttonsPanel.add(subtractButton);
+        buttonsPanel.add(multiplyButton);
+        buttonsPanel.add(divideButton);
+        buttonsPanel.add(modulusButton);
+        JButton[] digitButtons = new JButton[10];
+        for (int i = 0; i < 10; i++) {
+            digitButtons[i] = new JButton(String.valueOf(i));
+            digitButtons[i].addActionListener(this);
+            buttonsPanel.add(digitButtons[i]);
+        }
+        buttonsPanel.add(equalsButton);
         frame.add(textField);
-        frame.add(clearButton);
-        for (int i = 0; i < 10; i++)
-            frame.add(digitButtons[i]);
-        frame.add(addButton);
-        frame.add(subtractButton);
-        frame.add(multiplyButton);
-        frame.add(divideButton);
-        frame.add(modulusButton);
-        frame.add(equalsButton);
+        frame.add(buttonsPanel);
         frame.setVisible(true);
+    }
+
+    boolean isOperator(String str) {
+        switch (str) {
+            case "+":
+            case "-":
+            case "*":
+            case "/":
+            case "%":
+                return true;
+        }
+        return false;
+    }
+
+    int precedence(String str) {
+        switch (str) {
+            case "+":
+            case "-":
+                return 1;
+            case "*":
+            case "/":
+            case "%":
+                return 2;
+        }
+        return 0;
+    }
+
+    double evaluate(double y, double x, String op) {
+        switch (op) {
+            case "+":
+                return x + y;
+            case "-":
+                return x - y;
+            case "*":
+                return x * y;
+            case "/":
+                if (y == 0) {
+                    throw new ArithmeticException();
+                }
+                return x / y;
+            case "%":
+                return x % y;
+        }
+        return 0;
+    }
+
+    double evaluateExpression(String expression) {
+        Stack<String> operators = new Stack<>();
+        Stack<Double> operands = new Stack<>();
+        StringTokenizer tokenizer = new StringTokenizer(expression, "+-*/%", true);
+        while (tokenizer.hasMoreTokens()) {
+            String token = tokenizer.nextToken();
+            if (isOperator(token)) {
+                while ((!operators.isEmpty()) && precedence(operators.peek()) > precedence(token))
+                    operands.push(evaluate(operands.pop(), operands.pop(), operators.pop()));
+                operators.push(token);
+            } else {
+                operands.push(Double.parseDouble(token));
+            }
+        }
+        while (!operators.isEmpty())
+            operands.push(evaluate(operands.pop(), operands.pop(), operators.pop()));
+
+        if (!operands.isEmpty())
+            return operands.pop();
+        return 0;
     }
 
     public void actionPerformed(ActionEvent e) {
         JButton source = (JButton) e.getSource();
         if (source == equalsButton) {
-            String expression = textField.getText();
             try {
-                double result = evaluateExpression(expression);
+                double result = evaluateExpression(textField.getText());
                 textField.setText(String.valueOf(result));
-                isReset = false;
             } catch (ArithmeticException ex) {
                 textField.setText("Error: Divide by zero");
             }
-        } else if (source == clearButton) {
-            textField.setText("");
         } else {
-            if (!isReset) {
-                textField.setText("");
-                isReset = true;
-            }
             textField.setText(textField.getText() + source.getText());
         }
     }
-
-    double evaluateExpression(String expression) {
-        double operand1 = 0;
-        double operand2 = 0;
-        char operator = ' ';
-        boolean isOperand1 = true;
-
-        for (char term : expression.toCharArray()) {
-            if (term == '+' || term == '-' || term == '*' || term == '/' || term == '%') {
-                operator = term;
-                isOperand1 = false;
-            } else {
-                Double digit = (double) Character.getNumericValue(term);
-                if (isOperand1)
-                    operand1 = operand1 * 10 + digit;
-                else
-                    operand2 = operand2 * 10 + digit;
-
-            }
-        }
-
-        switch (operator) {
-            case '+':
-                return operand1 + operand2;
-            case '-':
-                return operand1 - operand2;
-            case '*':
-                return operand1 * operand2;
-            case '/':
-                if (operand2 == 0) {
-                    throw new ArithmeticException();
-                }
-                return operand1 / operand2;
-            case '%':
-                return operand1 % operand2;
-
-        }
-        return 0;
-    }
-
 }
 
 public class SimpleCalculator {
